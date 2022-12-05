f98a045977b6ba27e926fcd5ea192f56e765ed9e
Stefan Naewe <stefan.naewe@gmail.com>
2022-12-05 16:28:48 +0100

    restore pins_RAMPS.h from upstream
    Why has this been changed?
    
    Signed-off-by: Stefan Naewe <stefan.naewe@gmail.com>

--- Marlin/src/pins/ramps/pins_RAMPS.h
+++ Marlin/src/pins/ramps/pins_RAMPS.h
@@ -652,12 +652,10 @@
         #define DOGLCD_MOSI          EXP1_03_PIN
         #define DOGLCD_SCK           EXP1_05_PIN
       #else
-//        #define LCD_PINS_RS          EXP1_04_PIN         // DIGA-Tech:
-        #define LCD_PINS_RS          EXP2_03_PIN           // DIGA-Tech:
-        #define LCD_PINS_ENABLE      EXP1_03_PIN           // DIGA-Tech:
-        #define LCD_PINS_D4          EXP1_05_PIN           // DIGA-Tech:
-//        #define LCD_PINS_D5          EXP1_06_PIN         // DIGA-Tech:
-        #define LCD_PINS_D5          EXP2_05_PIN           // DIGA-Tech:
+        #define LCD_PINS_RS          EXP1_04_PIN
+        #define LCD_PINS_ENABLE      EXP1_03_PIN
+        #define LCD_PINS_D4          EXP1_05_PIN
+        #define LCD_PINS_D5          EXP1_06_PIN
         #define LCD_PINS_D6          EXP1_07_PIN
       #endif
 
@@ -695,16 +693,13 @@
         #define BTN_EN1              EXP1_03_PIN
         #define BTN_EN2              EXP1_05_PIN
       #else
-//        #define BTN_EN1              EXP2_03_PIN         // DIGA-Tech:
-//        #define BTN_EN2              EXP2_05_PIN         // DIGA-Tech:
-        #define BTN_EN1              EXP1_04_PIN           // DIGA-Tech:
-        #define BTN_EN2              EXP1_06_PIN           // DIGA-Tech:
+        #define BTN_EN1              EXP2_03_PIN
+        #define BTN_EN2              EXP2_05_PIN
       #endif
 
-      #define BTN_ENC                EXP1_02_PIN           // DIGA-Tech:
+      #define BTN_ENC                EXP1_02_PIN
       #ifndef SD_DETECT_PIN
-//        #define SD_DETECT_PIN        EXP2_07_PIN         // DIGA-Tech:
-        #define SD_DETECT_PIN        -1                    // DIGA-Tech:
+        #define SD_DETECT_PIN        EXP2_07_PIN
       #endif
       #ifndef KILL_PIN
         #define KILL_PIN             EXP2_08_PIN
